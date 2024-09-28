19cb8b1c7621abe14bc7084c50e683077da9c3af
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-09-28 18:01:59 +0600

    feat: added objects for services

--- src/config/site-data.en.js
+++ src/config/site-data.en.js
@@ -1,12 +1,43 @@
-
-const data ={
-    company:{
-        name: "Zeta-Stream"
+const data = {
+  company: {
+    name: "Zeta-Stream",
+  },
+  hero: {
+    title: "One stop solution for all your software needs",
+    subtitle:
+      "We have dedicated team of people who have worked with world's top companies and have delivered top notch products. And you can be next.",
+  },
+  services: [
+    {
+      title: "Responsive Web Development",
+      category: "Development",
+      description:
+        "We build fully fledged websites and web applications with responsive across all devices. We guided and advised by our senior developers who have half decades of experiences in this field and also provide optimized, efficient website with a very standard UI/UX design.",
     },
-    hero:{
-        title: "One stop solution for all your software needs",
-        subtitle: "We have dedicated team of people who have worked with world's top companies and have delivered top notch products. And you can be next.",
-    }
-}
+    {
+      title: "Mobile App Development",
+      description:
+        "We build native mobile apps for both iOS and Android platforms. We also provide cross-platform solutions with clean UI/UX design.",
+      category: "Development",
+    },
+    {
+      name: "Performance Optimization",
+      description:
+        "Improve application speed and efficiency by our advanced techniques. We have high experienced engineers and team of experts.",
+      category: "Optimization",
+    },
+    {
+      title: "SEO",
+      description:
+        "Like performance optimization, UI/UX design we focus on search engine optimization (SEO) for your website. Which helps your website to appear higher in search results.",
+    },
+    {
+      title: "Cloud Services",
+      category: "Services",
+      description:
+        "We mostly providing cloud services for your applications. Which includes AWS, GCP, Azure etc and also manage and optimize cloud-based infrastructures.",
+    },
+  ],
+};
 
 export default data;
